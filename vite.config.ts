b4d79d910c93bf9cd1e368614ff3ac130a964e84
doragonabe <32182997+doragonabe@users.noreply.github.com>
2022-06-24 16:43:13 +0900

    html圧縮

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import {viteStaticCopy} from 'vite-plugin-static-copy';
+import {createHtmlPlugin} from 'vite-plugin-html';
 import path from 'path';
 
 // https://vitejs.dev/config/
@@ -18,6 +19,9 @@ export default defineConfig({
                 src: path.resolve(__dirname, './static') + '/[!.]*',
                 dest: './',
             }],
+        }),
+        createHtmlPlugin({
+            minify: true,
         }),
     ],
 });
