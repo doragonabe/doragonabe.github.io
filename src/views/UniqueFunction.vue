aaa7595e02e87dce63993c585fcfe9a64b833928
doragonabe <32182997+doragonabe@users.noreply.github.com>
2023-05-03 17:56:49 +0900

    サイト更新

--- src/views/UniqueFunction.vue
+++ src/views/UniqueFunction.vue
@@ -23,7 +23,8 @@
                     <h4>作った機能</h4>
                     <ul style="list-style: none;">
                         <li>
-                            <table style="border: 1px black solid;">
+                            <hr>
+                            <table>
                                 <tr>
                                     <th>便利つるはし</th>
                                 </tr>
@@ -31,7 +32,7 @@
                                     <td>つるはしを右クリックで１×２マスのブロックを掘る機能に切り替えられる</td>
                                 </tr>
                                 <tr>
-                                    <td style="border: 1px black solid;">
+                                    <td>
                                         <span>発動条件</span>
                                         <ol>
                                             <li><span>つるはしを右クリックして機能がオンになっていること</span></li>
@@ -52,7 +53,8 @@
                             </table>
                         </li>
                         <li>
-                            <table style="border: 1px black solid; margin-top: 20px;">
+                            <hr>
+                            <table style="margin-top: 20px;">
                                 <tr>
                                     <th>リード機能拡張</th>
                                 </tr>
@@ -60,7 +62,7 @@
                                     <td>リードを右クリックで様々な便利機能を呼び出すGUIを表示する</td>
                                 </tr>
                                 <tr>
-                                    <td style="border: 1px black solid;">
+                                    <td>
                                         機能一覧
                                         <ol>
                                             <li>MyPetプラグインでよく使用されるコマンドをクリック一つで呼び出せる</li>
@@ -75,10 +77,55 @@
                                 </tr>
                             </table>
                         </li>
+                        <li>
+                            <hr>
+                            <table style="margin-top: 20px;">
+                                <tr>
+                                    <th>装飾用かまど</th>
+                                </tr>
+                                <tr>
+                                    <td>通常のかまどを装飾用に常時燃えている状態にさせることが出来ます。</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        使い方
+                                        <ol>
+                                            <li>ファイヤーチャージを持って消灯状態のかまどに向かって右クリック</li>
+                                        </ol>
+                                    </td>
+                                </tr>
+                            </table>
+                        </li>
+                        <li>
+                            <hr>
+                            <table style="margin-top: 20px;">
+                                <tr>
+                                    <th>クリエ飛行</th>
+                                </tr>
+                                <tr>
+                                    <td>時間制限ありでクリエイティブモードと同じ飛行を行うことが出来ます。</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        使い方
+                                        <ol>
+                                            <li>
+                                                <span>羽を持った状態で右クリックすると、５分間クリエ飛行が可能になる</span>
+                                            </li>
+                                            <li>羽は複数使用で効果時間がプラスで延長</li>
+                                            <li>
+                                                <span>落下速度低下のエフェクトが切れると効果を失います</span><br>
+                                                <span>そのため、時間切れ以外にも牛乳を飲むと効果を解除できます。</span>
+                                            </li>
+                                        </ol>
+                                    </td>
+                                </tr>
+                            </table>
+                        </li>
                     </ul>
                 </div>
             </div>
-            <h3>RecipeManager</h3>
+            <h3>RecipeManager/CustomCrafting</h3>
             <div class="h3_contents">
                 <p>
                     <router-link to="/original_recipes">独自レシピ</router-link>
@@ -88,7 +135,8 @@
             <h3>TreeAssist</h3>
             <div class="h3_contents">
                 <p>
-                    いろいろ
+                    <span>斧を持って木の最下層ブロックを破壊するだけで、自動的に木全体が伐採されます。</span><br>
+                    <span>これにより、手作業で木を伐採する手間や時間を省くことができます。</span>
                 </p>
             </div>
         </div>
