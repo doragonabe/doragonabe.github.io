d6daead4e26229e0e153d5359ffb37a573013fba
doragonabe <32182997+doragonabe@users.noreply.github.com>
2023-06-24 02:54:21 +0900

    fix

--- pages/test.vue
+++ pages/test.vue
@@ -1,9 +1,21 @@
 <template>
     <div>
-        <GoogleSignInButton @success="onSuccess" @error="onError"/>
+        <GoogleSignInButton @success="handleLoginSuccess" @error="handleLoginError"></GoogleSignInButton>
     </div>
 </template>
-<script lang="ts" setup>
-const onSuccess = (resp: any) => console.log(resp)
-const onError = (resp: any) => console.error(resp)
+<script setup lang="ts">
+import {GoogleSignInButton, type CredentialResponse, decodeCredential} from 'vue3-google-signin';
+
+// handle success event
+const handleLoginSuccess = (response: CredentialResponse) => {
+  const {credential} = response;
+  console.log('Access Token', credential);
+  const decodedCredential = decodeCredential(`${credential}`);
+  console.log("User:", decodedCredential);
+};
+
+// handle an error event
+const handleLoginError = () => {
+  console.error('Login failed');
+};
 </script>
