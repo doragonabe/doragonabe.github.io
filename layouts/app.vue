1dc3a9f6039a58eca39f36aae3d36847bccdd90c
doragonabe <32182997+doragonabe@users.noreply.github.com>
2023-06-26 20:32:32 +0900

    add

--- layouts/app.vue
+++ layouts/app.vue
@@ -92,7 +92,7 @@ ul.submenu {
 }
 
 </style>
-<script lang="ts">
+<script lang="ts" setup>
 //import 'sanitize.css';
 //import 'sanitize.css/assets.css';
 //import 'sanitize.css/forms.css';
@@ -108,96 +108,85 @@ import 'assets/css/craft.css';
 const header = ref<HTMLDivElement | null>(null);
 const sub = ref<HTMLDivElement | null>(null);
 
-export default {
-  setup() {
-    // window縦サイズ
-    const window_height = ref<number>(0);
-    // ヘッダー縦サイズ
-    const header_height = ref<number>(0);
-    // サイドメニュー縦サイズ
-    const sub_height = ref<number>(0);
+// window縦サイズ
+const window_height = ref<number>(0);
+// ヘッダー縦サイズ
+const header_height = ref<number>(0);
+// サイドメニュー縦サイズ
+const sub_height = ref<number>(0);
 
-    const resizeHandler = () => {
-      window_height.value = window.innerHeight;
-      header_height.value = header.value?.getBoundingClientRect().height ?? 0;
-      sub_height.value = sub.value?.getBoundingClientRect().height ?? 0;
+const resizeHandler = () => {
+  window_height.value = window.innerHeight;
+  header_height.value = header.value?.getBoundingClientRect().height ?? 0;
+  sub_height.value = sub.value?.getBoundingClientRect().height ?? 0;
 
-      if (window_height.value > sub_height.value + 40) { // windowサイズがメニューより大きい
-        sub.value?.classList.add('sticky_top');
-        sub.value?.classList.remove('sticky_bottom');
-      } else { // windowサイズがメニューより小さい
-        sub.value?.classList.remove('sticky_top');
-        sub.value?.classList.remove('sticky_bottom');
-      }
-    };
+  if (window_height.value > sub_height.value + 40) { // windowサイズがメニューより大きい
+    sub.value?.classList.add('sticky_top');
+    sub.value?.classList.remove('sticky_bottom');
+  } else { // windowサイズがメニューより小さい
+    sub.value?.classList.remove('sticky_top');
+    sub.value?.classList.remove('sticky_bottom');
+  }
+};
 
-    let scroll: number = 0;
-    const scrollHandler = () => {
-      const scroll_top: number = window.scrollY;
-      const scroll_bottom = scroll_top + window_height.value;
+let scroll: number = 0;
+const scrollHandler = () => {
+  const scroll_top: number = window.scrollY;
+  const scroll_bottom: number = scroll_top + window_height.value;
 
-      // サブメニューtopの位置
-      const sub_top: number = (sub.value?.getBoundingClientRect().top ?? 0) + scroll_top;
+  // サブメニューtopの位置
+  const sub_top: number = (sub.value?.getBoundingClientRect().top ?? 0) + scroll_top;
 
-      // サブメニューbottomの位置
-      const sub_bottom = sub_top + sub_height.value;
+  // サブメニューbottomの位置
+  const sub_bottom: number = sub_top + sub_height.value;
 
-      // サブメニュー初期位置と現在topの差
-      let sub_top_diff = sub_top - header_height.value - 50;
+  // サブメニュー初期位置と現在topの差
+  let sub_top_diff: number = sub_top - header_height.value - 50;
 
-      if (scroll_top < scroll) { //上スクロールの時の処理
-        if (window_height.value < sub_height.value + 40) { // windowサイズがメニューより小さい
-          if (sub_top >= scroll_top + 20) {
-            if (!sub.value?.classList.contains('sticky_top')) {
-              sub.value?.classList.add('sticky_top');
-              sub.value?.style.removeProperty('margin-top');
-            }
-          } else {
-            if (sub.value?.classList.contains('sticky_bottom')) {
-              sub.value?.style.setProperty('margin-top', String(sub_top_diff) + 'px');
-              sub.value?.classList.remove('sticky_bottom');
-            }
-          }
+  if (scroll_top < scroll) { //上スクロールの時の処理
+    if (window_height.value < sub_height.value + 40) { // windowサイズがメニューより小さい
+      if (sub_top >= scroll_top + 20) {
+        if (!sub.value?.classList.contains('sticky_top')) {
+          sub.value?.classList.add('sticky_top');
+          sub.value?.style.removeProperty('margin-top');
         }
       } else {
-        //下スクロールの時の処理
-        if (window_height.value < sub_height.value + 40) { // windowサイズがメニューより小さい
-          if (sub_top >= scroll_top + 20) {
-            if (sub.value?.classList.contains('sticky_top')) {
-              sub.value?.classList.remove('sticky_top');
-              sub.value?.style.setProperty('margin-top', String(sub_top_diff) + 'px');
-              sub.value?.style.setProperty('margin-top', String(sub_top_diff) + 'px');
-            }
-          } else {
-            if (sub_bottom + 20 <= scroll_bottom && !sub.value?.classList.contains('sticky_bottom')) { // scrollがサインドメニューのbottomを超えた場合
-              // scrollがサインドメニューのbottomを超えた場合
-              sub.value?.classList.remove('sticky_top');
-              sub.value?.classList.add('sticky_bottom');
-            }
-          }
+        if (sub.value?.classList.contains('sticky_bottom')) {
+          sub.value?.style.setProperty('margin-top', String(sub_top_diff) + 'px');
+          sub.value?.classList.remove('sticky_bottom');
         }
       }
-      scroll = scroll_top;
-    };
-
-    onMounted(() => {
-      resizeHandler();
-      window.addEventListener('resize', resizeHandler);
-      window.addEventListener('scroll', scrollHandler);
-    });
-
-    onUnmounted(() => {
-      window.removeEventListener('resize', resizeHandler);
-      window.removeEventListener('scroll', scrollHandler);
-    });
-
-    return {
-      header,
-      sub,
-      window_height,
-      header_height,
-    };
-  },
+    }
+  } else {
+    //下スクロールの時の処理
+    if (window_height.value < sub_height.value + 40) { // windowサイズがメニューより小さい
+      if (sub_top >= scroll_top + 20) {
+        if (sub.value?.classList.contains('sticky_top')) {
+          sub.value?.classList.remove('sticky_top');
+          sub.value?.style.setProperty('margin-top', String(sub_top_diff) + 'px');
+          sub.value?.style.setProperty('margin-top', String(sub_top_diff) + 'px');
+        }
+      } else {
+        if (sub_bottom + 20 <= scroll_bottom && !sub.value?.classList.contains('sticky_bottom')) { // scrollがサインドメニューのbottomを超えた場合
+          // scrollがサインドメニューのbottomを超えた場合
+          sub.value?.classList.remove('sticky_top');
+          sub.value?.classList.add('sticky_bottom');
+        }
+      }
+    }
+  }
+  scroll = scroll_top;
 };
+
+onMounted(() => {
+  resizeHandler();
+  window.addEventListener('resize', resizeHandler);
+  window.addEventListener('scroll', scrollHandler);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', resizeHandler);
+  window.removeEventListener('scroll', scrollHandler);
+});
 </script>
 
