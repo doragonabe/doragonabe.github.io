2cdc289782ad777957fa5750ade4514a7eaf55ff
doragonabe <32182997+doragonabe@users.noreply.github.com>
2023-06-27 20:18:29 +0900

    キャッシュ時間延長

---
+++ middleware/cache-control.ts
@@ -0,0 +1,12 @@
+import {IncomingMessage, ServerResponse} from 'http';
+
+export default defineEventHandler(event => {
+  const res: ServerResponse<IncomingMessage> = event.node.res;
+  const max_age: number = 3600; // 1時間
+  const url: string = event.node.req.url ?? '';
+  if (url) {
+    if (url.match(/(.+)\.(.+)\.(css|js|webp)/)) {
+      res.setHeader('Cache-Control', `max-age=${max_age}`);
+    }
+  }
+});
